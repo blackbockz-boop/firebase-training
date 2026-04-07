841e56944dda30ccddbde04730e11aa4944c4fb4
blackbockz-boop <blackbockz@gmail.com>
2026-04-07 10:10:56 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { auth } from './firebase/init';
+import { createPost, getAllPosts } from './firebase/post';
 import { createUserWithEmailAndPassword, 
   signInWithEmailAndPassword,
   onAuthStateChanged,
@@ -10,6 +11,7 @@ import { createUserWithEmailAndPassword,
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+  const [posts, setPosts] = React.useState([]);
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -52,12 +54,39 @@ function App() {
       });
   }
 
+  async function handleCreatePost() {
+    try {
+      await createPost();
+      const allPosts = await getAllPosts();
+      setPosts(allPosts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleGetAllPosts() {
+    try {
+      const allPosts = await getAllPosts();
+      setPosts(allPosts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
       {loading ? 'Loading...' : user.email}
+      <button onClick={handleCreatePost}>Create Post</button>
+      <button onClick={handleGetAllPosts}>Get All Posts</button>
+      {posts.map((post) => (
+        <div key={post.title}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/firebase/post.js
+++ src/firebase/post.js
@@ -2,21 +2,23 @@ import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "./init";
 import React from "react";
 
+export async function createPost() {
+  const post = {
+    title: "Land a $400k job",
+    description: "Finish Frontend Simplified",
+  };
+
+  await addDoc(collection(db, "posts"), post);
+}
+
+export async function getAllPosts() {
+  const querySnapshot = await getDocs(collection(db, "posts"));
+  const posts = querySnapshot.docs.map((doc) => doc.data());
+  console.log(posts);
+  return posts;
+}
+
 function Post() {
-  function createPost() {
-    const post = {
-      title: "Land a $400k job",
-      description: "Finish Frontend Simplified",
-    };
-    addDoc(collection(db, "posts"), post);
-  }
-
-  async function getAllPosts() {
-    const querySnapshot = await getDocs(collection(db, "posts"));
-    const posts = querySnapshot.docs.map((doc) => doc.data());
-    console.log(posts);
-  }
-
   return (
     <div>
       <button onClick={createPost}>Create Post</button>
